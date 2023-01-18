e70595ebbe6af8d08f2c8cb615c370fda041c23e
mmcgregor29 <mmcgregor@hawk.iit.edu>
2023-01-18 14:57:27 -0600

    Initial Commit

---
+++ SphereBoundingBox2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SphereBoundingBox2
+//
+//  Created by IIT Phys 440 on 1/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SphereBoundingBox2/SphereBoundingBox2App.swift
@@ -0,0 +1,17 @@
+//
+//  SphereBoundingBox2App.swift
+//  SphereBoundingBox2
+//
+//  Created by IIT Phys 440 on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SphereBoundingBox2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
